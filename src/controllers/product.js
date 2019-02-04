b309d8da5c55c2e906be7c3befd2ba01084c6b1b
Darko Milutinovic <milutinovic@optimal-systems.de>
2019-02-04 13:20:19 +0100

    product routes and controller

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -11,3 +11,51 @@ exports.getAllProducts = async (req, res) => {
         res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err);
     }
 };
+
+exports.getProductsByCategory = async (req, res) => {
+    try {
+        const categoryId = req.params.categoryId;
+        if (!categoryId) {
+            console.log('No categoryId supplied.');
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).send('No categoryId supplied.');
+        }
+
+        const products = await Product.find({ categoryId });
+        res.status(httpStatus.OK).json(products);
+    } catch(err) {
+        console.log(err);
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err);
+    }
+};
+
+exports.getProductsBySubCategory = async (req, res) => {
+    try {
+        const subCategoryId = req.params.subCategoryId;
+        if (!subCategoryId) {
+            console.log('No subCategoryId supplied.');
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).send('No subCategoryId supplied.');
+        }
+
+        const products = await Product.find({ subCategoryId });
+        res.status(httpStatus.OK).json(products);
+    } catch(err) {
+        console.log(err);
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err);
+    }
+};
+
+exports.getProduct = async (req, res) => {
+    try {
+        const productId = req.params.productId;
+        if (!productId) {
+            console.log('No productId supplied.');
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).send('No productId supplied.');
+        }
+
+        const products = await Product.findOne({ _id: productId });
+        res.status(httpStatus.OK).json(products);
+    } catch(err) {
+        console.log(err);
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err);
+    }
+};
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,10 +2,10 @@ const ProductController = require('../controllers/product');
 
 module.exports = api => {
     api.route('/products').get(ProductController.getAllProducts);
-    // api.route('/products/by-category/:categoryId').get(ProductController.getProductsByCategory);
-    // api.route('/products/by-sub-category/:subCategoryId').get(ProductController.getProductsBySubCategory);
+    api.route('/products/by-category/:categoryId').get(ProductController.getProductsByCategory);
+    api.route('/products/by-sub-category/:subCategoryId').get(ProductController.getProductsBySubCategory);
     // api.route('/products').post(ProductController.addProduct);
-    // api.route('/products/:productId').get(ProductController.getProduct);
+    api.route('/products/:productId').get(ProductController.getProduct);
     // api.route('/products/:productId').put(ProductController.editProduct);
     // api.route('/products/:productId').delete(ProductController.deleteProduct);
 };
