056d463bc16c9d7d26b612aa431069f9faf9a296
Darko Milutinovic <milutinovic@optimal-systems.de>
2019-02-07 16:35:35 +0100

    added get product list route

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,11 +1,28 @@
 // const validator = require('validator');
 const httpStatus = require('http-status');
 const Product = require('../models/product');
+const { ProductCategory } = require('../models/product-categories');
 
 exports.getAllProducts = async (req, res) => {
     try {
         const products = await Product.find({}).sort({ name: 1 }).populate('categoryId').populate('subCategoryId');
         res.status(httpStatus.OK).json(products);
+    } catch(err) {
+        console.log(err);
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err);
+    }
+};
+
+exports.getProductList = async (req, res) => {
+    try {
+        let productList = {};
+        const categories = await ProductCategory.find({}, '_id name').sort({ name: 1 });
+        for (let i = 0; i <= categories.length - 1; i++) {
+            const products = await Product.find({ categoryId: categories[i] }, '_id name decription subCategoryId allergens priceWithVAT isRecommended isPromo').populate('subCategoryId').sort({ 'subCategoryId.name': 1 });
+            productList[categories[i]] = products;
+        }
+        
+        res.status(httpStatus.OK).json(productList);
     } catch(err) {
         console.log(err);
         res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err);
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,6 +4,7 @@ module.exports = api => {
     api.route('/products').get(ProductController.getAllProducts);
     api.route('/products/by-category/:categoryId').get(ProductController.getProductsByCategory);
     api.route('/products/by-sub-category/:subCategoryId').get(ProductController.getProductsBySubCategory);
+    api.route('/products/list').get(ProductController.getProductList);
     // api.route('/products').post(ProductController.addProduct);
     api.route('/products/:productId').get(ProductController.getProduct);
     // api.route('/products/:productId').put(ProductController.editProduct);
