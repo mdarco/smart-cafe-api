7cb9f5dd6ee39e4d4fb60c4d1e2cebe31b1ce9f9
Darko Milutinović <dmilutinovic@gmail.com>
2019-02-24 17:11:01 +0100

    added get table support

--- src/routes/tables.js
+++ src/routes/tables.js
@@ -2,5 +2,6 @@ const TableController = require('../controllers/table');
 
 module.exports = api => {
     api.route('/tables').get(TableController.getAllTables);
+    api.route('/tables/:tableId').get(TableController.getTable);
     api.route('/tables/:tableId').put(TableController.updateTable);
 };
